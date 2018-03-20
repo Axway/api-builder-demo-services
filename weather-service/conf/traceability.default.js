0f50d39596a985dd4971f14d900146407fdba4b5
Gavin Matthews <gmatthews@axway.com>
2018-03-20 17:44:19 +0000

    Add traceability headers

---
+++ weather-service/conf/traceability.default.js
@@ -0,0 +1,11 @@
+/**
+ * Configuration used by the GreetFlow. 'saluation' is accessed via '$.helloworld.salutation'
+ * similarly any Arrow.config parameter can be accessed.
+ */
+const TRACEABILITY_HEADERS = process.env.TRACEABILITY_HEADERS || 'x-request-id, x-b3-traceid, x-b3-spanid, x-b3-parentspanid, x-b3-sampled, x-b3-flags, x-ot-span-context'
+
+module.exports = {
+	traceability: {
+		headers: TRACEABILITY_HEADERS.split(',').map(h=>h.trim())
+	}
+};
